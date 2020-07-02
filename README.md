58c3452135f5ef6063a056f3cf888c2aa02d9b44
Gellish <40665645+Gellish@users.noreply.github.com>
2020-07-02 15:38:56 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pharma_cms
+ 
---
+++ application/views/include/admin_footer.php
@@ -0,0 +1,10 @@
+<?php
+    $CI =& get_instance();
+    $CI->load->model('Web_settings');
+    $Web_settings = $CI->Web_settings->retrieve_setting_editdata();
+?>
+<footer class="main-footer">
+    <strong>
+    	
+   	</strong><i class="fa fa-heart color-green"></i>
+</footer>
